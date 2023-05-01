291f9acb3e1e3bb83a2fe2d05eabf03c45466038
Alessandro Joshua Zamproni <zmproni@gmail.com>
2023-05-01 14:52:31 +0800

    Create bare navigation page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
-	import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
-
-	onMount(() => {
-		goto('/word-cloud');
-	});
 </script>
+
+<main class='p-16'>
+	<p class='m-16'>
+		<a href='/word-cloud'>Word Cloud using Svelte and D3</a>
+	</p>
+	<p class='m-16'>
+		<a href='/bar-chart'>Free Code Camp D3 Bar Chart</a>
+	</p>
+</main>
